8b2602c142bf9b2338b573dce7f73aa83b984d3c
Tatyana-STEPANOVA <163852394+Tatyana-STEPANOVA@users.noreply.github.com>
2024-04-15 17:32:27 +0300

    Add files via upload

--- autotest.py
+++ autotest.py
@@ -6,7 +6,7 @@ def test_save_order_track():
     order_track = sendrequest.new_order(data.order_body).json()["track"]
     return order_track
 
-    # 3. Получение заказа по треку
+    # Получение заказа по треку
 def test_take_order_by_orders_track():
     response = sendrequest.get_info_order(sendrequest.order_track)
 
